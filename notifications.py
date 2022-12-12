3774f870a9c3652d890559ed869a890c662fccad
Daniel <74107489+ilkiyani@users.noreply.github.com>
2022-12-11 20:56:30 -0800

    connect to lsl and push data sample

--- notifications.py
+++ notifications.py
@@ -69,33 +69,3 @@ class XsensNotificationDelegate(btle.DefaultDelegate):
                 time.sleep(0.01)
         # now send it and wait for a bit before trying again.
         print(formatted_data)
-
-        srate = 60
-        name = 'Dot'
-        type = 'XSENS'
-        n_channels = 1
-    # first create a new stream info (here we set the name to BioSemi,
-    # the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
-    # last value would be the serial number of the device or some other more or
-    # less locally unique identifier for the stream as far as available (you
-    # could also omit it but interrupted connections wouldn't auto-recover)
-        info = StreamInfo(name, type, n_channels, srate, 'float32', 'myuid34234')
-
-    # next make an outlet
-        outlet = StreamOutlet(info)
-
-        print("now sending data...")
-        start_time = local_clock()
-        sent_samples = 0
-        while True:
-                elapsed_time = local_clock() - start_time
-                required_samples = int(srate * elapsed_time) - sent_samples
-                for sample_ix in range(required_samples):
-            # make a new random n_channels sample; this is converted into a
-            # pylsl.vectorf (the data type that is expected by push_sample)
-                        mysample = formatted_data
-            # now send it
-                        outlet.push_sample(mysample)
-                sent_samples += required_samples
-        # now send it and wait for a bit before trying again.
-                time.sleep(0.01)
