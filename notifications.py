e87af6d18453cbb5fb24b05623464d1dcc9173b9
Daniel <74107489+ilkiyani@users.noreply.github.com>
2022-12-12 09:10:29 -0800

    move lsl to server.py

--- notifications.py
+++ notifications.py
@@ -31,41 +31,4 @@ class XsensNotificationDelegate(btle.DefaultDelegate):
                        ('quat_y', np.float32), ('quat_z', np.float32)])
         human_readable_data = np.frombuffer(data, dtype=data_segments)
         formatted_data = str([x for x in human_readable_data.tolist()[0]][:-1])[1:-1]
-        srate = 60
-        name = 'Dot'
-        type = 'XSENS'
-        n_channels= 4
-    # first create a new stream info (here we set the name to BioSemi,
-    # the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
-    # last value would be the serial number of the device or some other more or
-    # less locally unique identifier for the stream as far as available (you
-    # could also omit it but interrupted connections wouldn't auto-recover)
-        info = StreamInfo(name, type, n_channels, srate, 'float32', 'myuid34234')
-
-    # next make an outlet
-        outlet = StreamOutlet(info)
-        start_time = local_clock()
-        sent_samples = 0
-        while True:
-                elapsed_time = local_clock() - start_time
-                required_samples = int(srate * elapsed_time) - sent_samples
-                for sample_ix in range(required_samples):
-                        index=0
-                        mysample=[]
-                        for x in range(n_channels):
-                            sample=str("")
-                            while formatted_data[index]!=",":
-                                sample+=formatted_data[index]
-                                if index==len(formatted_data)-1:
-                                    break
-                                index+=1
-                            sample=float(sample)
-                            mysample.append(sample)
-                            if index!=len(formatted_data)-1:
-                                index+=1
-            # now send it
-                        outlet.push_sample(mysample)
-                sent_samples += required_samples
-                time.sleep(0.01)
-        # now send it and wait for a bit before trying again.
         print(formatted_data)
